301e08ba44feb030e578f7b441ee37de752802a7
ismailgo95 <mail.kerja1995@gmail.com>
2022-09-05 15:20:06 +0700

    fix Humberger

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
           </div>
           <ul class="navbar-nav  justify-content-end">
             <li class="nav-item d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0" id="iconNavbarSidenav">
+              <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
                 <i class="fa fa-user me-sm-1"></i>
                 <span class="d-sm-inline d-none">Karumkit</span>
 
