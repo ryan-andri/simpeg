d999d51f744337df0f40e1946bacacca490b9750
ismailgo95 <mail.kerja1995@gmail.com>
2022-09-06 10:12:23 +0700

    Kesangean

--- index.php
+++ index.php
@@ -138,8 +138,8 @@ include_once('includes/header.php');
   <footer class="footer py-4  ">
     <div class="container-fluid">
       <div class="row align-items-center justify-content-lg-between">
-        <div class="col-lg-6 mb-lg-0 mb-4">
-          <div class="copyright text-center text-sm text-muted text-lg-start">
+        <div class="col-lg-6 mb-lg-0">
+          <div class="copyright text-center text-sm text-muted text-lg-start position-fixed bottom-0 mb-4 ">
             © <script>
               document.write(new Date().getFullYear())
             </script>,
