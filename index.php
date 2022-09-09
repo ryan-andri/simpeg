62f6784046f6b5d51df1c743058271296abe66e4
ismailgo95 <mail.kerja1995@gmail.com>
2022-09-09 08:37:26 +0700

    update formpegawai

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include_once('includes/header.php');
+include_once('includes/sidebar.php');
 ?>
 
 <!-- Content Wrapper. Contains page content -->
---
+++ pages/formpegawai.php
@@ -0,0 +1,10 @@
+<?php
+require_once('../configs/default.php');
+include_once('../includes/header.php');
+?>
+
+
+
+<?php
+include_once('../includes/footer.php');
+?>
