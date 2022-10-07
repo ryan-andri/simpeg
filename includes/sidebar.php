1565886825a98097223969e1ec820c1863e87af0
ismailgo95 <mail.kerja1995@gmail.com>
2022-10-07 07:32:34 +0700

    update Beranda

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -46,7 +46,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="index3.html" class="brand-link">
-        <img src="<?= BASE_URL ?>/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= BASE_URL ?>/assets/img/hesti.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">SIMPEG ONLINE</span>
       </a>
 
